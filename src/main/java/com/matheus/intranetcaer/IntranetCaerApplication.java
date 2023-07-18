55f83c1490630f778055e5c538f6c621eef6e0c8
Matheus Iam <matheusiam27@gmail.com>
2023-07-18 19:16:21 -0400

    initial commit

---
+++ src/main/java/com/matheus/intranetcaer/IntranetCaerApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.intranetcaer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetCaerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetCaerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/intranetcaer/IntranetCaerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.intranetcaer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetCaerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
