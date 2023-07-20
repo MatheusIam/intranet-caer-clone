07b65fa0fbf8b786ad0d6a7fbca9d67757e20267
Matheus Iam <matheusiam27@gmail.com>
2023-07-20 10:45:21 -0400

    removido código inutil

--- src/test/java/com/matheus/intranetcaer/restcontrollers/UsuarioRC.java
+++ src/test/java/com/matheus/intranetcaer/restcontrollers/UsuarioRC.java
@@ -2,7 +2,6 @@ package com.matheus.intranetcaer.restcontrollers;
 
 import java.net.URI;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
